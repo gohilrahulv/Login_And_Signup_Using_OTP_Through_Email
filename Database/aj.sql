339b5118c0a251748641d4ed9cb8329bf9cc0db5
gohil rahul <118652650+gohilrahulv@users.noreply.github.com>
2024-04-04 23:28:45 +0530

    Add files via upload

--- Database/aj.sql
+++ Database/aj.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 04, 2024 at 07:42 PM
+-- Generation Time: Apr 04, 2024 at 07:54 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.1.17
 
@@ -20,6 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Database: `aj`
 --
+CREATE DATABASE IF NOT EXISTS `aj` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `aj`;
 
 -- --------------------------------------------------------
 
@@ -27,8 +29,9 @@ SET time_zone = "+00:00";
 -- Table structure for table `my`
 --
 
-CREATE TABLE `my` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `my`;
+CREATE TABLE IF NOT EXISTS `my` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `fname` varchar(255) NOT NULL,
   `lname` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
@@ -36,28 +39,9 @@ CREATE TABLE `my` (
   `address` varchar(255) NOT NULL,
   `gender` varchar(255) NOT NULL,
   `pass` varchar(255) NOT NULL,
-  `login_status` varchar(255) NOT NULL DEFAULT 'pending'
+  `login_status` varchar(255) NOT NULL DEFAULT 'pending',
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `my`
---
-ALTER TABLE `my`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `my`
---
-ALTER TABLE `my`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
